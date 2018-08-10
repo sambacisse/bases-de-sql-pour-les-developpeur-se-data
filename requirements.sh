d5d7dcaa212ece25667bcaa8f49766499709a87f
Filip Schouwenaars <filip@datacamp.com>
2018-08-10 15:05:54 +0200

    clean up requirements file

--- requirements.sh
+++ requirements.sh
@@ -1,44 +1,23 @@
 # If bash command fails, build should error out
 set -e
 
-BASE_DIR=$(pwd)
-#NYCBIKES_DATA="https://assets.datacamp.com/course/sql/nycbikes15.zip"
-#NYCBIKES_REPO="https://assets.datacamp.com/course/tmp_fixme_filip/nycbikes15_db_scripts.zip"
-FILMS_REPO="https://assets.datacamp.com/course/tmp_fixme_filip/films.zip"
-
+# Get the data zip and unpack
 apt-get update && apt-get install -y unzip
-# get repos
-#wget $NYCBIKES_REPO
-#unzip nycbikes15_db_scripts.zip
-
+BASE_DIR=$(pwd)
+FILMS_REPO="https://assets.datacamp.com/course/tmp_fixme_filip/films.zip"
 mkdir -p courses-intro-to-sql/data
 wget $FILMS_REPO
 unzip films.zip -d courses-intro-to-sql/data
+rm films.zip
 
-#git clone https://$GITHUB_TOKEN@github.com/datacamp/courses-intro-to-sql.git
-#git clone https://$GITHUB_TOKEN@github.com/datacamp/nycbikes15.git
-
-# get data for nycbikes15
-#wget $NYCBIKES_DATA
-#unzip nycbikes15.zip -d ./nycbikes15/data
-
-# build databases
-#service postgresql start \
-  #&& sudo -u postgres createdb -O repl films \
-  #&& sudo -u postgres createdb -O repl nycbikes15 \
-  #&& cd courses-intro-to-sql \
-  #&& sudo -u postgres psql films < data/films/films.sql \
-  #&& cd $BASE_DIR/nycbikes15 \
-  #&& sudo -u postgres psql nycbikes15 < sql-setup/create-db-postgres.sql \
-  #&& service postgresql stop
-
-pip3 install jinja2==2.10
-pip3 install git+https://github.com/datacamp/protowhat.git/@fs/refactor --upgrade --no-dependencies
-pip3 install git+https://github.com/datacamp/sqlwhat.git/@fs/refactor --upgrade --no-dependencies
-pip3 install sqlwhat-ext==0.0.1 --upgrade --no-dependencies
-
+# Load the database into postgreSQL
 service postgresql start \
   && sudo -u postgres createdb -O repl films \
   && cd courses-intro-to-sql \
   && sudo -u postgres psql films < data/films/films.sql \
   && service postgresql stop
+
+# override installs from sql-shared
+pip3 install jinja2==2.10
+pip3 install git+https://github.com/datacamp/protowhat.git/@fs/refactor --upgrade --no-dependencies
+pip3 install git+https://github.com/datacamp/sqlwhat.git/@fs/refactor --upgrade --no-dependencies
