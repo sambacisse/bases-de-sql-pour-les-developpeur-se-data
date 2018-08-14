644ed8afce6a649abec6be432c240c36c66c2b1b
Filip Schouwenaars <filip@datacamp.com>
2018-08-14 13:35:54 +0200

    get rid of BASEDIR

--- requirements.sh
+++ requirements.sh
@@ -3,7 +3,6 @@ set -e
 
 # Get the data zip and unpack
 apt-get update && apt-get install -y unzip
-BASE_DIR=$(pwd)
 FILMS_REPO="https://assets.datacamp.com/course/tmp_fixme_filip/films.zip"
 mkdir -p courses-intro-to-sql/data
 wget $FILMS_REPO
