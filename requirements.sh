d0d674b975e369f31feb5dc8f97c296ea1b425ba
Samba Cissé <contact@sambacisse.com>
2019-10-27 04:20:59 +0100

    Update requirements.sh from editor

--- requirements.sh
+++ requirements.sh
@@ -4,9 +4,9 @@ set -e
 # Get the data zip and unpack
 apt-get update && apt-get install -y unzip
 FILMS_REPO="https://assets.datacamp.com/course/tmp_fixme_filip/films.zip"
-mkdir -p courses-intro-to-sql/data
+mkdir -p bases-de-sql-pour-les-developpeur-se-data/data
 wget $FILMS_REPO
-unzip films.zip -d courses-intro-to-sql/data
+unzip films.zip -d bases-de-sql-pour-les-developpeur-se-data/data
 rm films.zip
 
 # Load the database into postgreSQL
